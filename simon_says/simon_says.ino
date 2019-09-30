fdc60e0fb5cc7d9c5dab07351de3d01344a6c65b
Mark Kvetny <biowaffeln@gmail.com>
2019-10-01 00:48:51 +0100

    name ports with define

--- simon_says/simon_says.ino
+++ simon_says/simon_says.ino
@@ -1,3 +1,13 @@
+#define RESTART_INPUT_PORT 8
+
+#define GREEN_INPUT_PORT 1
+#define ORANGE_INPUT_PORT 2
+#define RED_INPUT_PORT 3
+
+#define GREEN_OUTPUT_PORT 10
+#define ORANGE_OUTPUT_PORT 11
+#define RED_OUTPUT_PORT 12
+
 enum State
 {
   idle,
@@ -24,19 +34,19 @@ void setup()
   level = Level::level1;
 
   /* LED Inputs */
-  pinMode(1, INPUT);
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+  pinMode(GREEN_INPUT_PORT, INPUT);
+  pinMode(ORANGE_INPUT_PORT, INPUT);
+  pinMode(RED_INPUT_PORT, INPUT);
 
   /* restart input */
-  pinMode(8, INPUT);
+  pinMode(RESTART_INPUT_PORT, INPUT);
 
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
-  digitalWrite(12, LOW);
+  pinMode(GREEN_OUTPUT_PORT, OUTPUT);
+  pinMode(ORANGE_OUTPUT_PORT, OUTPUT);
+  pinMode(RED_OUTPUT_PORT, OUTPUT);
+  digitalWrite(GREEN_OUTPUT_PORT, LOW);
+  digitalWrite(ORANGE_OUTPUT_PORT, LOW);
+  digitalWrite(RED_OUTPUT_PORT, LOW);
 
   /* for debugging */
   // Serial.begin(9600);
@@ -53,21 +63,21 @@ void loop()
   /* start state */
   if (gameState == State::start)
   {
-    digitalWrite(10, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(12, HIGH);
+    digitalWrite(GREEN_OUTPUT_PORT, HIGH);
+    digitalWrite(ORANGE_OUTPUT_PORT, HIGH);
+    digitalWrite(RED_OUTPUT_PORT, HIGH);
     delay(500);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
+    digitalWrite(GREEN_OUTPUT_PORT, LOW);
+    digitalWrite(ORANGE_OUTPUT_PORT, LOW);
+    digitalWrite(RED_OUTPUT_PORT, LOW);
     delay(500);
-    digitalWrite(10, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(12, HIGH);
+    digitalWrite(GREEN_OUTPUT_PORT, HIGH);
+    digitalWrite(ORANGE_OUTPUT_PORT, HIGH);
+    digitalWrite(RED_OUTPUT_PORT, HIGH);
     delay(500);
-    digitalWrite(10, LOW);
-    digitalWrite(11, LOW);
-    digitalWrite(12, LOW);
+    digitalWrite(GREEN_OUTPUT_PORT, LOW);
+    digitalWrite(ORANGE_OUTPUT_PORT, LOW);
+    digitalWrite(RED_OUTPUT_PORT, LOW);
     delay(1000);
     gameState = State::blinking;
   }
@@ -81,8 +91,8 @@ void loop()
   if (gameState == State::finish)
   {
     delay(500);
-    digitalWrite(10, HIGH);
-    digitalWrite(11, HIGH);
-    digitalWrite(12, HIGH);
+    digitalWrite(GREEN_OUTPUT_PORT, HIGH);
+    digitalWrite(ORANGE_OUTPUT_PORT, HIGH);
+    digitalWrite(RED_OUTPUT_PORT, HIGH);
   }
 }
