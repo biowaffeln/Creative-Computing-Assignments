c9f9a093eea6a3af0f6ee2e5028f4831e633288e
Mark Kvetny <biowaffeln@gmail.com>
2019-10-01 18:16:22 +0100

    comments and formatting

--- simon_says/simon_says.ino
+++ simon_says/simon_says.ino
@@ -17,35 +17,25 @@ int colorInputs[3] = {GREEN_OUTPUT_PORT, ORANGE_OUTPUT_PORT, RED_OUTPUT_PORT};
 
 const int NUM_LEVELS = 3;
 
-enum State
-{
-  idle,
-  start,
-  blinking,
-  input,
-  finish
-};
+enum State { idle, start, blinking, input, finish };
 
 State gameState;
 int level;
 vector<int> levelSequence;
 vector<int> userInput;
 
-/* helper functions */
+/* --- HELPER FUNCTIONS --- */
 
-vector<int> generateSequence(int currentLevel)
-{
+vector<int> generateSequence(int currentLevel) {
   int listLength = currentLevel + 2;
 
   vector<int> sequence;
   int prevItem;
   int i = 0;
 
-  while (i < listLength)
-  {
+  while (i < listLength) {
     int color = colorOutputs[rand() % 3];
-    if (color != prevItem)
-    {
+    if (color != prevItem) {
       sequence.push_back(color);
       prevItem = color;
       i++;
@@ -55,50 +45,43 @@ vector<int> generateSequence(int currentLevel)
   return sequence;
 }
 
-bool matching(vector<int> input, vector<int> sequence)
-{
+bool matching(vector<int> input, vector<int> sequence) {
   /* should never happen, but eh, better be safe */
-  if (input.size() > sequence.size())
-    return false;
+  if (input.size() > sequence.size()) return false;
 
-  for (int i = 0; i < input.size(); i++)
-  {
-    if (input[i] != sequence[i])
-    {
+  for (int i = 0; i < input.size(); i++) {
+    if (input[i] != sequence[i]) {
       return false;
     }
   }
   return true;
 }
 
-void powerAllOn()
-{
+void powerAllOn() {
   digitalWrite(GREEN_OUTPUT_PORT, HIGH);
   digitalWrite(ORANGE_OUTPUT_PORT, HIGH);
   digitalWrite(RED_OUTPUT_PORT, HIGH);
 }
 
-void powerAllOff()
-{
+void powerAllOff() {
   digitalWrite(GREEN_OUTPUT_PORT, LOW);
   digitalWrite(ORANGE_OUTPUT_PORT, LOW);
   digitalWrite(RED_OUTPUT_PORT, LOW);
 }
 
-void setup()
-{
+/* --- GAME LOGIC --- */
+
+void setup() {
   /* initial state */
   gameState = State::idle;
   level = 1;
-
   /* LED Inputs */
   pinMode(GREEN_INPUT_PORT, INPUT);
   pinMode(ORANGE_INPUT_PORT, INPUT);
   pinMode(RED_INPUT_PORT, INPUT);
-
   /* restart input */
   pinMode(RESTART_INPUT_PORT, INPUT);
-
+  /* LED Outputs */
   pinMode(GREEN_OUTPUT_PORT, OUTPUT);
   pinMode(ORANGE_OUTPUT_PORT, OUTPUT);
   pinMode(RED_OUTPUT_PORT, OUTPUT);
@@ -109,11 +92,9 @@ void setup()
   // Serial.begin(9600);
 }
 
-void loop()
-{
+void loop() {
   /* reset */
-  if (digitalRead(8) == HIGH)
-  {
+  if (digitalRead(8) == HIGH) {
     powerAllOff();
     level = 1;
     userInput.clear();
@@ -121,8 +102,7 @@ void loop()
   }
 
   /* start state */
-  if (gameState == State::start)
-  {
+  if (gameState == State::start) {
     powerAllOn();
     delay(500);
     powerAllOff();
@@ -136,11 +116,8 @@ void loop()
   }
 
   /* blinking */
-  if (gameState == State::blinking)
-  {
-
-    for (const auto &port : levelSequence)
-    {
+  if (gameState == State::blinking) {
+    for (const auto& port : levelSequence) {
       digitalWrite(port, HIGH);
       delay(500);
       digitalWrite(port, LOW);
@@ -150,18 +127,14 @@ void loop()
   }
 
   /* input */
-  if (gameState == State::input)
-  {
-    for (const auto &port : colorInputs)
-    {
-      if (digitalRead(port) == HIGH && (userInput.empty() || port != userInput.back()))
-      {
+  if (gameState == State::input) {
+    for (const auto& port : colorInputs) {
+      if (digitalRead(port) == HIGH &&
+          (userInput.empty() || port != userInput.back())) {
         userInput.push_back(port);
 
-        if (matching(userInput, levelSequence))
-        {
-          if (userInput.size() == levelSequence.size())
-          {
+        if (matching(userInput, levelSequence)) {
+          if (userInput.size() == levelSequence.size()) {
             // go to next level;
             userInput.clear();
             levelSequence.clear();
@@ -174,21 +147,16 @@ void loop()
             delay(200);
             powerAllOff();
 
-            if (level > NUM_LEVELS)
-            {
+            if (level > NUM_LEVELS) {
               delay(500);
               gameState = State::finish;
-            }
-            else
-            {
+            } else {
               delay(1000);
               levelSequence = generateSequence(level);
               gameState = State::blinking;
             }
           }
-        }
-        else
-        {
+        } else {
           // restart level;
           userInput.clear();
           powerAllOn();
@@ -202,8 +170,7 @@ void loop()
   }
 
   /* finish */
-  if (gameState == State::finish)
-  {
+  if (gameState == State::finish) {
     powerAllOn();
   }
 }
